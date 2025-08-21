35f3b1ff27395fe2eb0d7d8c3e575d7207faca73
Chevonne Serfontein <cmserfontein96@gmail.com>
2025-08-21 15:47:34 +0200

    set up connection to database

--- starter/server.js
+++ starter/server.js
@@ -1,7 +1,22 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB, {
+  // .connect(process.env.DATABASE_LOCAL, {} <-- if using local database
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('DB connection successful!');
+});
 
 // Port the server will listen on
 const port = process.env.PORT || 3000;
