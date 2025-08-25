ca11aec80ac96fb3373ee0f159f2976e1406cb96
Chevonne Serfontein <cmserfontein96@gmail.com>
2025-08-25 14:58:00 +0200

    feat: update getAllTours method to build and execute dynamic query based on request parameters

--- starter/controllers/tourController.js
+++ starter/controllers/tourController.js
@@ -14,8 +14,21 @@ exports.checkBody = (req, res, next) => {
 // GET request to fetch all tours
 exports. getAllTours = async (req, res) => {
   try {  
-    const tours = await Tour.find();
+    // BUILD THE QUERY
+    const queryObj = {...req.query};
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
 
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    console.log(JSON.parse(queryStr));
+    
+    const query = Tour.find(JSON.parse(queryStr));
+
+    // EXECUTE THE QUERY
+    const tours = await query;
+
+    // SEND THE RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
